1577aaf6415cb34d02e1ad87d6669698dd9f2f79
gligorov <vladimir.gligorov@cern.ch>
2017-11-09 15:14:43 +0100

    fix trivial bug

--- src/RapidIPSmearGauss.cc
+++ src/RapidIPSmearGauss.cc
@@ -7,7 +7,7 @@ std::pair<double,double> RapidIPSmearGauss::smearIP(double ip, double pt) {
 
     const double sigma_ = intercept_ + slope_/pt;
     const double smear_ = ip+gRandom->Gaus(0.,sigma_);
-    return std::pair<double,double>(sigma_,smear_);
+    return std::pair<double,double>(smear_,sigma_);
 
 }
 
