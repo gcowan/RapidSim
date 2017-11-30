4e9bbc94981b0053858c542e4160303f202cf20f
Greig Cowan <greig.cowan@gmail.com>
2017-11-30 16:13:30 +0000

    Fixed the scaling of the energy for photon smearing

--- src/RapidMomentumSmearEnergyGauss.cc
+++ src/RapidMomentumSmearEnergyGauss.cc
@@ -5,13 +5,14 @@
 #include <iostream>
 
 TLorentzVector RapidMomentumSmearEnergyGauss::smearMomentum(TLorentzVector p) {
-	double energy = p.E();
-	double first = stochastic_/TMath::Sqrt(energy);
-	first *= first;
-	double second = constant_*constant_;
-	double res = TMath::Sqrt(first + second)*energy;
-	double smearedEnergy = gRandom->Gaus(0,res) + energy;
-	p.SetPxPyPzE( p.Px(), p.Py(), p.Pz(), smearedEnergy );
+    double energy = p.E();
+    double first = stochastic_/TMath::Sqrt(energy*1000.);
+    first *= first;
+    double second = constant_*constant_;
+    double res = TMath::Sqrt(first + second)*energy;
+    double smearedEnergy = gRandom->Gaus(0,res) + energy;
+    double norm = smearedEnergy/energy;
+	p.SetPxPyPzE( p.Px()*norm, p.Py()*norm, p.Pz()*norm, smearedEnergy );
 	return p;
 }
 
